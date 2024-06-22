9c90658e73bc69789581cd4dff71ed10f047c405
Andy Sant <andy@clavus.io>
2024-06-22 13:35:28 +0200

    fix: single file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { ReturnTypeOverride, UnionOfFunctionReturnTypes } from "./types";
+type ExcludeTrue<T> = T extends true ? never : T;
+
+export type UnionOfFunctionReturnTypes<
+  FuncArray extends ((...args: any) => any)[]
+> = ExcludeTrue<Awaited<ReturnType<FuncArray[number]>>>;
+
+export type ReturnTypeOverride<
+  F extends (...args: any) => any,
+  O,
+  A = true
+> = true extends A ? Awaited<ReturnType<F>> | Awaited<O> : ReturnType<F> | O;
 
 export const Guard = <
   F extends (...args: any) => any,
@@ -21,4 +31,3 @@ export const Guard = <
     return await func(...args);
   };
 };
-
